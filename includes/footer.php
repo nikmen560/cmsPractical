e1b878bb2b7c14de819a9c3887cb9bd052000ba1
Nikita <kent720p@gmail.com>
2022-06-02 21:23:49 +0200

    created footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,25 +1,22 @@
-
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; CMS by KentHD</p>
-                </div>
-                <!-- /.col-lg-11 -->
-            </div>
-            <!-- /.row -->
-        </footer>
-
-    </div>
-    <!-- /.container -->
-
-    <!-- jQuery -->
+</div>
+<!-- container -->
+<footer class="footer-distributed">
+    <div class="container">
+			<div class="footer-right">
+				<a href="https://www.facebook.com/profile.php?id=100002154544876"><i class="fa fa-facebook"></i></a>
+				<a href="https://www.linkedin.com/in/mikita-kavaliou-0b04b4231/"><i class="fa fa-linkedin"></i></a>
+				<a href="https://github.com/nikmen560"><i class="fa fa-github"></i></a>
+			</div>
+			<div class="footer-left">
+				<p class="footer-links">
+					<a class="link-1" href="/cms">Home</a>
+					<a href="/cms/contact">Contact me</a>
+				</p>
+				<p>CMS Moto blog by Kent HD &copy; 2022</p>
+			</div>
+		</footer>
     <script src="/cms/js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
     <script src="/cms/js/bootstrap.min.js"></script>
     <script src="/cms/js/script.js"></script>
-
 </body>
-
 </html>
